b3900d1c5c3f197528336be795db3d29e1d4dc33
jackchu <jackchu.work@gmail.com>
2017-06-17 21:42:59 +0800

    修改VIEWMODLE

--- MVC_WORK_SESSION1/MVC_WORK/MVC_WORK/Models/ViewModels.cs
+++ MVC_WORK_SESSION1/MVC_WORK/MVC_WORK/Models/ViewModels.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Web;
 
@@ -11,8 +12,11 @@ namespace MVC_WORK.Models
 
     public class AccountingViewModel
     {
+        [DisplayName("金額")]
         public string MONEY { get; set; }
+        [DisplayName("日期")]
         public string DATE { get; set; }
+        [DisplayName("備註")]
         public string REMARKS { get; set; }
 
     }
