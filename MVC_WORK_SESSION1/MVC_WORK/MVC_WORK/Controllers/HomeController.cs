8dc7da3c46f5c476ee2a5b0256824e01148f5595
jackchu <jackchu.work@gmail.com>
2017-06-17 21:30:01 +0800

    1.Layout 新增Partial 2.新增ViewModels頁 3.新增__ForIndexPartialView.cshtml 4.修改HomeControl ==>Index 5.Home/Index.cshtml 畫面修改

--- MVC_WORK_SESSION1/MVC_WORK/MVC_WORK/Controllers/HomeController.cs
+++ MVC_WORK_SESSION1/MVC_WORK/MVC_WORK/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -10,6 +11,15 @@ namespace MVC_WORK.Controllers
     {
         public ActionResult Index()
         {
+            
+            List <Models.AccountingDataViewModel> AccountData = new List<Models.AccountingDataViewModel>();
+            AccountData.Add(new Models.AccountingDataViewModel { DATE = "106/06/01", MONEY = "1000", TYPE = "支出" });
+            AccountData.Add(new Models.AccountingDataViewModel { DATE = "106/06/03", MONEY = "1500", TYPE = "支出" });
+            AccountData.Add(new Models.AccountingDataViewModel { DATE = "106/06/13", MONEY = "500", TYPE = "支出" });
+
+            ViewData["ViewDataTest"] = AccountData;
+            ViewData["Page"] = "1";
+            ViewData["Size"] = "20";
             return View();
         }
 
---
+++ MVC_WORK_SESSION1/MVC_WORK/MVC_WORK/Models/ViewModels.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_WORK.Models
+{
+    public class ViewModels
+    {
+    }
+
+    public class AccountingViewModel
+    {
+        public string MONEY { get; set; }
+        public string DATE { get; set; }
+        public string REMARKS { get; set; }
+
+    }
+
+    public class AccountingDataViewModel
+    {
+        public string TYPE { get; set; }
+        public string DATE { get; set; }
+        public string MONEY { get; set; }
+       
+    }
+
+
+
+}
