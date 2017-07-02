bbad522031784694491a9f36040d340c0a6171ea
jackchu <jackchu.work@gmail.com>
2017-07-02 11:50:00 +0800

    前一版忘了打勾...

---
+++ MVC_WORK_SESSION1/MVC_WORK/MVC_WORK/ViewModel/Accounting.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace MVC_WORK.ViewModel
+{
+    public class Accounting
+    {
+    }
+
+
+    public class AccountingViewModel
+    {
+        [DisplayName("金額")]
+        public string MONEY { get; set; }
+        [DisplayName("日期")]
+        public string DATE { get; set; }
+        [DisplayName("備註")]
+        public string REMARKS { get; set; }
+
+    }
+
+    public class AccountingDataViewModel
+    {
+        [DisplayName("類別")]
+        public string AccountTYPE { get; set; }
+        [DisplayName("日期")]
+        public string DATE { get; set; }
+        [DisplayName("金額")]
+        public string MONEY { get; set; }
+
+    }
+
+}
