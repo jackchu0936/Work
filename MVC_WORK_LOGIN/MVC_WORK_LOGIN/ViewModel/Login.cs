c35fb52a2b5c91f6a9f54449826efce943cffbf9
jackchu <jackchu.work@gmail.com>
2017-07-12 17:39:51 +0800

    增加ViewModel(Login)

---
+++ MVC_WORK_LOGIN/MVC_WORK_LOGIN/ViewModel/Login.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC_WORK_LOGIN.ViewModel
+{
+    public class Login
+    {
+        [DisplayName("帳號")]
+        [RegularExpression(@"\w.+\@\w.+")]
+        public string LOGIN_ID { get; set; }
+        [DisplayName("密碼")]
+        [StringLength(20,MinimumLength =4)]
+        public string PASSWORD { get; set; }
+    }
+}
